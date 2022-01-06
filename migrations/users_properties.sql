d6508b21ba08a5b732199ce0aaccf6ba5eefbccf
JP <kalambayjp@gmail.com>
2022-01-06 09:49:12 -0800

    added premade testing data and fixed some mismatching names of columns

--- migrations/users_properties.sql
+++ migrations/users_properties.sql
@@ -7,22 +7,22 @@ CREATE TABLE users (
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
-  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  
   title VARCHAR(255) NOT NULL,
   description TEXT,
-  thumbnail_photo_url VARCHAR(255) NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  
   cover_photo_url VARCHAR(255) NOT NULL,
+  thumbnail_photo_url VARCHAR(255) NOT NULL,
   cost_per_night INTEGER NOT NULL DEFAULT 0,
   parking_spaces INTEGER NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
+
+  active BOOLEAN NOT NULL DEFAULT TRUE,
   
+  province VARCHAR(255) NOT NULL,
+  city VARCHAR(255) NOT NULL,
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  province VARCHAR(255) NOT NULL,
-  post_code VARCHAR(255) NOT NULL,
-  
-  is_active BOOLEAN NOT NULL DEFAULT TRUE
+  post_code VARCHAR(255) NOT NULL
 );
--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -7,26 +7,27 @@ VALUES
 ;
 
 INSERT INTO properties (
-  owner_id,
   title,
   description,
-  thumbnail_photo_url,
+  owner_id,
   cover_photo_url,
+  thumbnail_photo_url,
   cost_per_night,
   parking_spaces,
   number_of_bathrooms,
   number_of_bedrooms,
-  country, street,
-  city,
   province,
+  city,
+  country, 
+  street,
   post_code
 )
 VALUES 
-  (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 500, 2, 2, 4, 'Canada', '513 Powov Grove', 'Anmore', 'British Columbia', 'v7e2h8'),
+  ('Speed lamp','description', 1, 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 500, 2, 2, 4, 'British Colombia', 'Anmore', 'Canada', '513 Powov Grove', 'v7e2h8'),
 
-  (2, 'Blank corner', 'description', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg', 500, 2, 2, 4, 'Canada', '169 Nuwug Circle', 'Anmore', 'British Columbia', 'h4d7g5'),
+  ('Blank corner', 'description', 2, 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/1475938/pexels-photo-1475938.jpeg', 500, 2, 2, 4, 'British Colombia',  'Burnaby','Canada', '169 Nuwug Circle', 'h4d7g5'),
 
-  (2, 'Habit mix', 'description', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg', 500, 2, 2, 4, 'Canada', '834 Buwmi Road', 'Anmore', 'British Columbia', 'c6s9g6')
+  ('Habit mix', 'description', 2, 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2076739/pexels-photo-2076739.jpeg', 500, 2, 2, 4,'British Colombia', 'Delta',  'Canada', '834 Buwmi Road', 'c6s9g6')
 ;
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
