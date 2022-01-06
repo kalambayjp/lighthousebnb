92795e7cfedafb3ec0194bf58b5ca37d195946bb
JP <kalambayjp@gmail.com>
2022-01-05 17:46:25 -0800

    created users and properies tables

---
+++ migrations/users_properties.sql
@@ -0,0 +1,25 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  num_of_bathrooms INTEGER,
+  num_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  is_active BOOLEAN
+);
